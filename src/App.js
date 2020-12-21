e904a94abeca6bff823d448fe3e5f1e00e95f346
taka <71979704+nikotamajapan@users.noreply.github.com>
2020-12-22 01:23:57 +0900

    fire

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
+import React from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDLwIal6QUyU6G3dp6MCkZQzZfSbO8GMvk",
+  authDomain: "test3-91199.firebaseapp.com",
+  projectId: "test3-91199",
+  storageBucket: "test3-91199.appspot.com",
+  messagingSenderId: "754636685483",
+  appId: "1:754636685483:web:8a5b081f619baf6c04f7ee",
+  measurementId: "G-HJ6FRRB97H"
+};
+firebase.initializeApp(firebaseConfig);
+
+
 function App() {
+  const handleClickFetchButton =() =>{
+    const db = firebase.firestore();
+    db.collection('users').doc('8QdSeumAbMUysOhpO0ZZ').get().then((doc)=>{
+      console.log('Document data:', doc.data());
+    });
+
+    console.log('fetch clicked');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit and save to reload.123
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClickFetchButton}>aaa
+        </button>
+        
       </header>
     </div>
   );
